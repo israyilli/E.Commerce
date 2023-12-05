cd8952f27a64022a4d3316421cc4ef0d085de58d
israyilli <israyillismaye@gmail.com>
2023-12-05 19:52:46 +0400

    new commit samaya

--- src/Config/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "./BasketSlice";
-import wishlistReducer from "./WishlistSlice";
-export const store = configureStore({
-  reducer: {
-    basket: basketReducer,
-    wishlist: wishlistReducer,
-  },
-});
